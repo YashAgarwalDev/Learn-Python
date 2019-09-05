5977be1d14b3057d36374c941e32d2b86c70669e
Yash Agarwal <41363605+YashAgarwalDev@users.noreply.github.com>
2019-09-05 16:49:20 +0530

    Boolean operators
    The "and" and "or" boolean operators allow building complex boolean expressions

---
+++ BooleanOperators.py
@@ -0,0 +1,9 @@
+name = "Yash"
+age = 21
+ 
+  if name == "Yash" and age == 21
+     print("Your name is Yash, and you are also 21 years old.")
+     
+     
+  if name == "Yash" or name == "Rick"
+      print("you name is either Yash or Rick")
