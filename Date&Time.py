32d261292f7a0525e6acfbbd2af369d2c515581e
Yash Agarwal <41363605+YashAgarwalDev@users.noreply.github.com>
2019-09-14 16:58:51 +0530

    Gettting Date and Time stuff
    this code helps you to get Date and Time.
    As we all can make an idea that time intervals have to be represented in floating point number. The floating-point numbers in units of seconds are signified by Tick in python. Particular instants of time are represented in seconds since 12:00 am, 1st of January, of the year 1990. A popular module name 'time' is available which provides functions that let's programmer work with time and made conversion between time-representation possible

---
+++ Date&Time.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+presentime = datetime.now()
+
+print(" NOW THE TIME IS:", presentime)
+
+print("Todays Date is:", presentime.strftime('%Y-%m-%d') )
+
+print("Year is:", presentime.year)
+
+print("MOnth is:", presentime.month)
+
+print("Day is:", presentime.day)
